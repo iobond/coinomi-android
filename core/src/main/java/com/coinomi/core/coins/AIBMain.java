f436919a5229bd6edf132ab7004841402c2a1fbb
International Online Bond <aib@iobond.com>
2015-11-02 20:35:01 -0800

    Create AIBMain.java

---
+++ core/src/main/java/com/coinomi/core/coins/AIBMain.java
@@ -0,0 +1,35 @@
+package com.coinomi.core.coins;
+
+import com.coinomi.core.coins.families.BitFamily;
+
+/**
+ * @author John L. Jegutanis
+ */
+public class AIBMain extends CoinType {
+    private AIBMain() {
+        id = "aib.main";
+
+        addressHeader = 23;
+        p2shHeader = 5;
+        acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
+        spendableCoinbaseDepth = 100;
+        dumpedPrivateKeyHeader = 176;
+
+        family = BitFamily.get();
+        name = "AIB Bond";
+        symbol = "AIB";
+        uriScheme = "aibond";
+        bip44Index = 2;   //// TODO: 2015/10/18  apply bip44
+        unitExponent = 8;
+        feePerKb = value(100000);
+        minNonDust = value(1000); // 0.00001 LTC mininput
+        softDustLimit = value(100000); // 0.001 LTC
+        softDustPolicy = SoftDustPolicy.BASE_FEE_FOR_EACH_SOFT_DUST_TXO;
+        signedMessageHeader = toBytes("Asian Investment Bond Signed Message:\n");
+    }
+
+    private static AIBMain instance = new AIBMain();
+    public static synchronized AIBMain get() {
+        return instance;
+    }
+}
